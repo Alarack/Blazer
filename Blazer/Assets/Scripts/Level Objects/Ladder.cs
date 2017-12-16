678479fd0031963cb4164f2da833d4c88cb0e15c
Alarack <jcbelgard@gmail.com>
2017-12-15 23:34:06 -0600

    Quick an dirty ladders

---
+++ Blazer/Assets/Scripts/Level Objects/Ladder.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ladder : MonoBehaviour {
+
+    public float climbSpeed;
+    public float descendSpeed;
+
+    public List<string> validUserTags = new List<string>();
+
+    private Entity currentUser;
+    private float currentSpeed;
+    private bool isClimbing;
+    private Rigidbody2D currentBody;
+
+    private void Update() {
+        currentSpeed = Input.GetAxisRaw("Vertical") * climbSpeed;
+
+        if(currentUser != null && !isClimbing) {
+            if(currentSpeed!= 0f) {
+                isClimbing = true;
+                currentBody.gravityScale = 0f;
+                
+            }
+        }
+
+
+
+    }
+
+
+    private void FixedUpdate() {
+        if(currentUser != null && isClimbing) {
+            currentBody.velocity = new Vector2(currentBody.velocity.x, currentSpeed);
+            //currentUser.transform.position = new Vector3(transform.position.x, currentUser.transform.position.y, 0f);
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D other) {
+        if (validUserTags.Contains(other.gameObject.tag)) {
+            currentUser = other.gameObject.GetComponent<Entity>();
+            if (currentUser != null)
+                currentBody = currentUser.GetComponent<Rigidbody2D>();
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D other) {
+        if (validUserTags.Contains(other.gameObject.tag)) {
+            if (currentUser != null) {
+                isClimbing = false;
+                currentBody.gravityScale = 1f;
+                currentUser = null;
+                
+            }
+
+        }
+
+    }
+
+}
