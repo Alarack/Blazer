72ce4cefad2678ff9e2332de2b3de73f993ca4fb
Unknown <adeptusforgeceo@gmail.com>
2018-01-25 21:08:24 -0600

    Ladder Fixes and Re-implementation

--- Blazer/Assets/Scripts/Level Objects/Ladder.cs
+++ Blazer/Assets/Scripts/Level Objects/Ladder.cs
@@ -4,63 +4,40 @@ using UnityEngine;
 
 public class Ladder : MonoBehaviour {
 
-    public float climbSpeed;
-    public float descendSpeed;
+
 
     public List<string> validUserTags = new List<string>();
 
-    private Entity currentUser;
-    private float currentSpeed;
-    private bool isClimbing;
-    private Rigidbody2D currentBody;
+
 
     private void Update() {
-        if (Input.GetAxisRaw("Vertical") >= 0){
-            currentSpeed = Input.GetAxisRaw("Vertical") * climbSpeed;
-        }
-        else if (Input.GetAxisRaw("Vertical") <= 0){
-            currentSpeed = Input.GetAxisRaw("Vertical") * descendSpeed;
-        }
-
-            if (currentUser != null && !isClimbing) {
-            if(currentSpeed!= 0f) {
-                isClimbing = true;
-                currentBody.gravityScale = 0f;
-                
-            }
-        }
-
 
 
     }
+
+
+
 
 
     private void FixedUpdate() {
-        if(currentUser != null && isClimbing) {
-            currentBody.velocity = new Vector2(currentBody.velocity.x, currentSpeed);
-            //currentUser.transform.position = new Vector3(transform.position.x, currentUser.transform.position.y, 0f);
-        }
+
     }
 
-    private void OnTriggerEnter2D(Collider2D other) {
+    private void OnTriggerStay2D(Collider2D other) {
         if (validUserTags.Contains(other.gameObject.tag)) {
-            currentUser = other.gameObject.GetComponent<Entity>();
-            if (currentUser != null)
-                currentBody = currentUser.GetComponent<Rigidbody2D>();
+            other.gameObject.GetComponent<EntityMovement>().canClimb = true;
+            other.gameObject.GetComponent<EntityMovement>().currentLadder = this.gameObject;
         }
     }
 
     private void OnTriggerExit2D(Collider2D other) {
         if (validUserTags.Contains(other.gameObject.tag)) {
-            if (currentUser != null) {
-                isClimbing = false;
-                currentBody.gravityScale = 1f;
-                currentUser = null;
-                
-            }
 
+            other.gameObject.GetComponent<EntityMovement>().ClimbEnd();
+            other.gameObject.GetComponent<EntityMovement>().canClimb = false;
+            other.gameObject.GetComponent<EntityMovement>().currentLadder = null;
+            other.gameObject.GetComponent<EntityMovement>().ClimbEnd();
         }
-
     }
 
 }
