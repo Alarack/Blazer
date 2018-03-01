9a5dd0e06d3b1680e4e4b6b851230f01dcbc4355
Unknown <adeptusforgeceo@gmail.com>
2018-03-01 06:31:53 -0600

    Ladder Script Adjust for Laptop

--- Blazer/Assets/Scripts/Level Objects/AttemptToClimb.cs
+++ Blazer/Assets/Scripts/Level Objects/AttemptToClimb.cs
@@ -4,58 +4,32 @@ using UnityEngine;
 
 public class AttemptToClimb : MonoBehaviour {
 
-
-
     public Ladder myLadder;
-
     public Ladder.Climber myClimber;
-
-
 
     // Use this for initialization
 
     void Start()
     {
 
-
-
     }
-
-
 
     // Update is called once per frame
-
     void Update()
     {
-
         if (Input.GetKeyDown(KeyCode.UpArrow))
-
         {
-
             myLadder.GrabLadder(gameObject);
-
         }
-
         if (Input.GetKeyDown(KeyCode.RightArrow) && gameObject.name == "TestClimber")
-
         {
-
             transform.Translate(0, 1, 0);
-
             myLadder.Climb(myClimber, gameObject);
-
         }
-
         if (Input.GetKeyDown(KeyCode.LeftArrow) && gameObject.name == "TestClimber1")
-
         {
-
             transform.Translate(0, 1, 0);
-
             myLadder.Climb(myClimber, gameObject);
-
         }
-
     }
-
 }
--- Blazer/Assets/Scripts/Level Objects/Ladder.cs
+++ Blazer/Assets/Scripts/Level Objects/Ladder.cs
@@ -5,222 +5,100 @@ using UnityEngine;
 public class Ladder : MonoBehaviour {
 
     public float ladderLength;
-
     private List<Climber> myClimbers = new List<Climber>();
-
-
-
     public class Climber
-
     {
-
         public GameObject climbObject;
-
         public float climberLocation;
-
-
-
         public Climber(GameObject climbObject, float climberLocation)
-
         {
-
             this.climbObject = climbObject;
-
             this.climberLocation = climberLocation;
-
         }
-
     }
-
-
-
     private void Start()
-
     {
-
-
-
     }
-
-
-
-
-
     private void FixedUpdate()
-
     {
-
         //Debug.Log(myClimbers.Count + " is the number of current climbers");
-
         //Debug.Log(Vector2.Distance(transform.position, climber.transform.position));
-
         foreach (Climber climber in myClimbers)
-
         {
-
             //Debug.Log(climber.climbObject.name + " " + climber.climberLocation);
-
         }
-
     }
-
-
-
     public void GrabLadder(GameObject attemptedToGrab)
-
     {
-
         Climber tempClimb = new Climber(attemptedToGrab, TransformToLadderLocation(attemptedToGrab));
-
         //Debug.Log(attemptedToGrab + "attempted to grab ladder");
-
         myClimbers.Add(tempClimb);
-
         attemptedToGrab.GetComponent<AttemptToClimb>().myClimber = tempClimb;
-
     }
-
-
-
     public void Climb(Climber myClimber, GameObject me)
-
     {
-
         if (TransformToLadderLocation(me) <= ladderLength && TransformToLadderLocation(me) >= 0)
-
         {
-
             myClimber.climberLocation = TransformToLadderLocation(me);
-
         }
 
         else if (TransformToLadderLocation(me) > ladderLength)
-
         {
-
             LetGoLadder();
-
         }
 
         else if (TransformToLadderLocation(me) < 0)
-
         {
-
             LetGoLadder();
-
         }
-
         //Debug.Log(climbObject + "should be climbing");
-
     }
-
-
-
     public float TransformToLadderLocation(GameObject climbObject)
-
     {
-
         float transformLocation = Vector2.Distance(climbObject.transform.localPosition, transform.position);
-
         float ladderLocation = 0;
-
-
-
         //Debug.Log(climbObject + "" + transformLocation);
-
         if (transformLocation != 0)
-
         {
-
             if (Mathf.Min(climbObject.transform.position.y, transform.position.y) == climbObject.transform.position.y)
-
             {
-
                 ladderLocation = (ladderLength / 2f) - transformLocation;
-
                 //Debug.Log(ladderLocation);
-
             }
-
             else if (Mathf.Min(climbObject.transform.position.y, transform.position.y) == transform.position.y)
-
             {
-
                 ladderLocation = transformLocation + (ladderLength / 2f);
-
                 //Debug.Log(ladderLocation);
-
-
-
             }
-
         }
-
         else
-
         {
-
             ladderLocation = (ladderLength / 2f);
-
             //Debug.Log(ladderLocation);
-
         }
-
-
-
         return ladderLocation;
-
     }
-
-
-
     //private void OnTriggerStay2D(Collider2D other)
-
     //{
-
     //    if (validUserTags.Contains(other.gameObject.tag))
-
     //    {
-
     //        other.gameObject.GetComponent<EntityMovement>().canClimb = true;
-
     //        other.gameObject.GetComponent<EntityMovement>().currentLadder = this.gameObject;
-
     //    }
-
     //}
-
-
-
     //private void OnTriggerExit2D(Collider2D other)
-
     //{
-
     //    if (validUserTags.Contains(other.gameObject.tag))
-
     //    {
-
     //        other.gameObject.GetComponent<EntityMovement>().ClimbEnd();
-
     //        other.gameObject.GetComponent<EntityMovement>().canClimb = false;
-
     //        other.gameObject.GetComponent<EntityMovement>().currentLadder = null;
-
     //        other.gameObject.GetComponent<EntityMovement>().ClimbEnd();
-
     //    }
-
     //}
-
-
-
     public void LetGoLadder()
-
     {
-
         Debug.Log("A climber has let go of ladder");
-
     }
-
 }
 
